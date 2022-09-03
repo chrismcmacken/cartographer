d137f55d2b403aba767829d0b1a112479feeb418
Chris McMacken <chris.mcmacken@gmail.com>
2022-09-03 09:42:47 -0500

    Install prettier, format files, remove unused deps

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import L from 'leaflet';
+import L from "leaflet";
 
-let map = L.map('map').setView([74, -66], 3);
+let map = L.map("map").setView([74, -66], 3);
 
 L.tileLayer("sword_coast/{z}/{y}/{x}.webp", {
-    tileSize: 256,
-    crs: L.CRS.Simple,
-    minZoom: 3,
-    maxZoom: 6,
-    noWrap: true
+  tileSize: 256,
+  crs: L.CRS.Simple,
+  minZoom: 3,
+  maxZoom: 6,
+  noWrap: true,
 }).addTo(map);
